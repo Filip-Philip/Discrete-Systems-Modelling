b6c78e787432080a0c37b9dfad8154b7c05f9cea
DESKTOP-3759NNU\Filip <skalka.fil@gmail.com>
2022-03-24 20:42:12 +0100

    overtaking

--- lab3-cars 1/lab3-cars/src/Board.java
+++ lab3-cars 1/lab3-cars/src/Board.java
@@ -8,7 +8,7 @@ import java.awt.event.MouseEvent;
 public class Board extends JComponent implements MouseInputListener, ComponentListener {
     private static final long serialVersionUID = 1L;
     private Point[][] points;
-    private int size = 10;
+    private int size = 25;
     public int editType = 0;
 
     public Board(int length, int height) {
@@ -24,18 +24,33 @@ public class Board extends JComponent implements MouseInputListener, ComponentLi
 
         for (int x = 0; x < points.length; ++x) {
             for (int y = 0; y < points[x].length; ++y) {
-                points[x][y] = new Point();
+                points[x][y] = new Point((y+1) % 2);
+            }
+        }
+
+        for (int x = 0; x <= points.length - 1; ++x) {
+            for (int y = 0; y < 2; ++y) {
+                points[x][y].setType(5);
+            }
+            for (int y = 4; y < 6; ++y) {
+                points[x][y].setType(5);
             }
         }
 
         for (int x = 0; x < points.length - 1; x++) {
-            for (int y = 0; y < points[x].length; y++) {
-                points[x][y].setNext(points[x + 1][y]);
+            for (int y = 2; y < 4; y++) {
+                points[x][y].setNextRight(points[x + 1][3]);
+                points[x + 1][y].setPreviousRight(points[x][3]);
+                points[x][y].setNextLeft(points[x + 1][2]);
+                points[x + 1][y].setPreviousLeft(points[x][2]);
             }
         }
 
-        for (int y = 0; y < points[points.length - 1].length; y++) {
-            points[points.length - 1][y].setNext(points[0][y]);
+        for (int y = 2; y < 4; y++) {
+            points[points.length - 1][y].setNextRight(points[0][3]);
+            points[points.length - 1][y].setNextLeft(points[0][2]);
+            points[0][y].setPreviousRight(points[points.length - 1][3]);
+            points[0][y].setPreviousLeft(points[points.length - 1][2]);
         }
     }
 
@@ -47,10 +62,11 @@ public class Board extends JComponent implements MouseInputListener, ComponentLi
         }
 
         for (int x = 0; x < points.length; ++x) {
-            for (int y = 0; y < points[x].length; ++y) {
+            for (int y = 2; y < 4; ++y) {
                 points[x][y].accelerate();
                 points[x][y].slowDown();
-                points[x][y].randomlySlowDown();
+                points[x][y].returnToLane();
+                points[x][y].overtake();
                 points[x][y].move();
             }
         }
@@ -97,11 +113,12 @@ public class Board extends JComponent implements MouseInputListener, ComponentLi
         for (x = 0; x < points.length; ++x) {
             for (y = 0; y < points[x].length; ++y) {
                 float a = 1.0F;
-                if (points[x][y].getType() == 1) {
-                    g.setColor(new Color(0.0f, 0.0f, 0.0f, 0.7f));
-                }
-                else if (points[x][y].getType() == 0) {
-                    g.setColor(new Color(1.0f, 1.0f, 1.0f, 0.7f));
+                switch (points[x][y].getType()) {
+                    case 0 -> g.setColor(new Color(1.0f, 1.0f, 1.0f, a));
+                    case 1 -> g.setColor(new Color(0.0f, 1.0f, 1.0f, a));
+                    case 2 -> g.setColor(new Color(0.0f, 0.0f, 1.0f, a));
+                    case 3 -> g.setColor(new Color(1.0f, 0.0f, 0.0f, a));
+                    case 5 -> g.setColor(new Color(0.0f, 1.0f, 0.0f, a));
                 }
                 g.fillRect((x * size) + 1, (y * size) + 1, (size - 1), (size - 1));
             }
@@ -110,15 +127,23 @@ public class Board extends JComponent implements MouseInputListener, ComponentLi
     }
 
     public void mouseClicked(MouseEvent e) {
+        clickOrClear(e);
+    }
+
+    private void clickOrClear(MouseEvent e) {
         int x = e.getX() / size;
         int y = e.getY() / size;
-        if ((x < points.length) && (x > 0) && (y < points[x].length) && (y > 0)) {
-            if (editType == 0) {
+        if ((x < points.length) && (x >= 0) && (y < points[x].length) && (y >= 0)) {
+            if(editType==0){
                 points[x][y].clicked();
+            }
+            else {
+                points[x][y].setType(editType);
             }
             this.repaint();
         }
     }
+
 
     public void componentResized(ComponentEvent e) {
         int dlugosc = (this.getWidth() / size) + 1;
@@ -127,14 +152,7 @@ public class Board extends JComponent implements MouseInputListener, ComponentLi
     }
 
     public void mouseDragged(MouseEvent e) {
-        int x = e.getX() / size;
-        int y = e.getY() / size;
-        if ((x < points.length) && (x > 0) && (y < points[x].length) && (y > 0)) {
-            if (editType == 0) {
-                points[x][y].clicked();
-            }
-            this.repaint();
-        }
+        clickOrClear(e);
     }
 
     public void mouseExited(MouseEvent e) {
--- lab3-cars 1/lab3-cars/src/Point.java
+++ lab3-cars 1/lab3-cars/src/Point.java
@@ -1,15 +1,22 @@
 import java.util.Random;
 
 public class Point {
+    public static Integer[] types = {0, 1, 2, 3, 5};
     private final float PROBABILITY_OF_RANDOM_SLOWDOWN = 0.1f;
-    private final int MAX_SPEED = 5;
+    private final int MAX_SPEED_TYPE_1 = 3;
+    private final int MAX_SPEED_TYPE_2 = 5;
+    private final int MAX_SPEED_TYPE_3 = 7;
     private int type;
-    private Point next;
+    private Point nextRight;
+    private Point nextLeft;
+    private Point previousRight;
+    private Point previousLeft;
     private boolean moved = false;
     private int speed = 0;
+    private int lane;
 
-    public Point(){
-
+    public Point(int lane){
+        this.lane = lane;
     }
 
     public void setMoved(boolean moved) { this.moved = moved; }
@@ -18,37 +25,148 @@ public class Point {
 
     public int getType() { return type; }
 
-    public void setNext(Point next) { this.next = next; }
+    public void setNextRight(Point nextRight) { this.nextRight = nextRight; }
+
+    public void setNextLeft(Point nextLeft) { this.nextLeft = nextLeft; }
+
+    public void setPreviousRight(Point previousRight) { this.previousRight = previousRight; }
+
+    public void setPreviousLeft(Point previousLeft) { this.previousLeft = previousLeft; }
+
+    private void setSpeed(int speed) { this.speed = speed; }
+
+    private boolean isCar() {
+        return type == 1 || type == 2 || type == 3;
+    }
 
     public void move() {
         Point car = this;
-        for (int i = 0; i < speed; i++) car = car.next;
+        for (int i = 0; i < speed; i++) car = car.nextRight;
 
-        if (type == 1 && !moved && car.getType() == 0) {
+        if (isCar() && !moved && car.getType() == 0) {
+            car.setType(type);
             type = 0;
-            car.setType(1);
             moved = true;
             car.setMoved(true);
+            car.setSpeed(speed);
+        }
+    }
+
+    private int getMaxSpeedForType() {
+        return switch (type) {
+            case 1 -> MAX_SPEED_TYPE_1;
+            case 2 -> MAX_SPEED_TYPE_2;
+            case 3 -> MAX_SPEED_TYPE_3;
+            default -> -1;
+        };
+    }
+
+    public void overtake() {
+        if (!moved && lane == 0 && speed < getMaxSpeedForType() && getDistanceToPreviousCar(0) >= getMaxSpeedOfPreviousCar(0)
+            && getDistanceToPreviousCar(1) >= getMaxSpeedOfPreviousCar(1) && getDistanceToNextCar(1) >= speed) {
+            Point car = this;
+            for (int i = 0; i < speed - 1; i++) car = car.nextLeft;
+
+            if (isCar() && car.getType() == 0) {
+                car.setType(type);
+                type = 0;
+                moved = true;
+                car.setMoved(true);
+                car.setSpeed(speed + 1);
+            }
+        }
+    }
+
+    public void returnToLane() {
+        if (!moved && lane == 1 && getDistanceToPreviousCar(0) >= getMaxSpeedOfPreviousCar(0)
+                && getDistanceToPreviousCar(1) >= getMaxSpeedOfPreviousCar(1) && getDistanceToNextCar(0) >= speed) {
+            Point car = this;
+            for (int i = 0; i < speed - 1; i++) car = car.nextRight;
+
+            if (isCar() && car.getType() == 0) {
+                car.setType(type);
+                type = 0;
+                moved = true;
+                car.setMoved(true);
+                car.setSpeed(speed);
+            }
         }
     }
 
     public void accelerate() {
-        if (speed < MAX_SPEED) speed++;
+        if (type == 1 && speed < MAX_SPEED_TYPE_1) speed++;
+        else if (type == 2 && speed < MAX_SPEED_TYPE_2) speed++;
+        else if (type == 3 && speed < MAX_SPEED_TYPE_3) speed++;
     }
 
-    public int getDistanceToNextCar() {
+    private int getMaxSpeedOfPreviousCar(int lane) {
+        Point car = this;
+        int i = 0;
+        if (lane == 0) {
+            car = car.previousRight;
+            while (!car.isCar() && i <= MAX_SPEED_TYPE_3) {
+                car = car.previousRight;
+                i++;
+            }
+        }
+        else if (lane == 1) {
+            car = car.nextLeft;
+            while (!car.isCar() && i <= MAX_SPEED_TYPE_3) {
+                car = car.nextLeft;
+                i++;
+            }
+        }
+        return car.getMaxSpeedForType();
+    }
+
+    private int getDistanceToNextCar(int lane) {
         int distance = 0;
         Point car = this;
-        car = car.next;
-        while (car.getType() != 1) {
-            car = car.next;
-            distance++;
+        int i = 0;
+        if (lane == 0) {
+            car = car.nextRight;
+            while (!car.isCar() && i <= MAX_SPEED_TYPE_3) {
+                car = car.nextRight;
+                distance++;
+                i++;
+            }
+        }
+        else if (lane == 1) {
+            car = car.nextLeft;
+            while (!car.isCar() && i <= MAX_SPEED_TYPE_3) {
+                car = car.nextLeft;
+                distance++;
+                i++;
+            }
+        }
+        return distance;
+    }
+
+    private int getDistanceToPreviousCar(int lane) {
+        int distance = 0;
+        Point car = this;
+        int i = 0;
+        if (lane == 0) {
+            car = car.previousRight;
+            while (!car.isCar() && i <= MAX_SPEED_TYPE_3) {
+                car = car.previousRight;
+                distance++;
+                i++;
+            }
+        }
+        else if (lane == 1) {
+            car = car.previousLeft;
+            while (!car.isCar() && i <= MAX_SPEED_TYPE_3) {
+                car = car.previousLeft;
+                distance++;
+                i++;
+            }
         }
         return distance;
     }
 
     public void slowDown() {
-        if (type == 1 && getDistanceToNextCar() < speed) speed = getDistanceToNextCar();
+        if (isCar() && getDistanceToNextCar(lane) < speed) speed = getDistanceToNextCar(lane);
     }
 
     public void randomlySlowDown() {
@@ -60,7 +178,7 @@ public class Point {
     }
 
     public void clicked() {
-        type = 1;
+        type = 0;
     }
 
     public void clear() {
